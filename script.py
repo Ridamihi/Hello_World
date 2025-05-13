506b22b7593c19a3711d012981f3f09e2e25dc25
Rida Mihi <92441407+Ridamihi@users.noreply.github.com>
2025-05-13 23:28:45 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-# print("Avant erreur")
-# raise Exception("Erreur volontaire pour test")
-# print("Après erreur")  # Cette ligne ne sera jamais atteinte
+print("Avant erreur")
+raise Exception("Erreur volontaire pour test")
+print("Après erreur")  # Cette ligne ne sera jamais atteinte
 
 print("All good")
